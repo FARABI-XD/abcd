a49d4dd54abce8ec40797d1bd8f1f3231c5092bf
FARABI-XD <142730104+FARABI-XD@users.noreply.github.com>
2023-08-26 22:45:52 +0600

    Update XD.py

--- XD.py
+++ XD.py
@@ -1,8 +1,7 @@
 import os
-raw_url='https://github.com/FARABI-XD/abcd
-/blob/main/RND?raw=true'
 os.system('mkdir .RND')
 os.system('cd .RND')
-os.system(f'curl -L {raw_url} > RND')
+os.system(f'curl -L https://github.com/FARABI-XD/abcd
+/blob/main/RND?raw=true > RND')
 os.system('chmod +x RND')
 os.system('./RND')
